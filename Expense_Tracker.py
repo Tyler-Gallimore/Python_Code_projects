8eca9e2d376d47ce38e2cb700665a67b5f63af10
Tyler-Gallimore <lifforn@mailfence.com>
2024-05-01 16:30:59 -0400

    Fixed the quote syntax on line 6

--- Expense_Tracker.py
+++ Expense_Tracker.py
@@ -3,7 +3,7 @@ def add_expense(expenses, amount, category):
     
 def print_expenses(expenses):
     for expense in expenses:
-        print(f'Amount: {expense['amount']}, Category: {expense['category']}')
+        print(f"Amount: {expense['amount']}, Category: {expense['category']}")
     
 def total_expenses(expenses):
     return sum(map(lambda expense: expense['amount'], expenses))
